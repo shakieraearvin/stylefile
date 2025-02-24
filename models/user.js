7e0e45e39f3babeaa204b9667342cc6e49b3d2a8
Shakiera Earvin <kieraearvin@gmail.com>
2025-02-24 15:38:50 -0500

    Built homepageand added middleware

--- models/user.js
+++ models/user.js
@@ -1,5 +1,43 @@
 const mongoose = require('mongoose');
 
+const reviewSchema = mongoose.Schema({
+  bodySize: {
+    type: String,
+  },
+  bodyHeight: {
+    type: String,
+  },
+  bodyWeight: {
+    type: String,
+  },
+  bodyType: {
+    type: String,
+    enum: ['hourglass', 'rectangle', 'inverted triangle', 'pear', 'apple'],
+  },
+  itemName: {
+    type: String,
+    required: true,
+  },
+  itemSize: {
+    type: String,
+    required: true,
+  },
+  itemReview: {
+    type: String,
+  },
+  itemLink: {
+    type: String,
+  },
+  recommendation: {
+    type: String,
+    enum: ['purchase', 'do not purchase', 'still deciding'],
+  },
+  itemPicture: {
+    type: String,
+  },
+
+});
+
 const userSchema = mongoose.Schema({
   username: {
     type: String,
@@ -9,6 +47,7 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  reviews: [reviewSchema],
 });
 
 const User = mongoose.model('User', userSchema);
