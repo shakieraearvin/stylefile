12b20c23a381414fd1cceca57d689be3a164bf01
Shakiera Earvin <kieraearvin@gmail.com>
2025-02-24 20:35:27 -0500

    added create functionality

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -20,5 +20,24 @@ router.get('/new', async (req, res) => {
   res.render('reviews/new.ejs');
 });
 
+//POST /users/:userId/reviews
+router.post('/', async (req, res) => {
+  try {
+    // Look up the user from req.session
+    const currentUser = await User.findById(req.session.user._id);
+    // Push req.body (the new form data object) to the
+    // applications array of the current user
+    currentUser.applications.push(req.body);
+    // Save changes to the user
+    await currentUser.save();
+    // Redirect back to the applications index view
+    res.redirect(`/users/${currentUser._id}/applications`);
+  } catch (error) {
+    // If any errors, log them and redirect back home
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
 
 module.exports = router;
