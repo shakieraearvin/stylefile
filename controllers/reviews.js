c88112e10c8cd140bd9a50ddc3fa484a0bb78c06
Shakiera Earvin <kieraearvin@gmail.com>
2025-02-24 23:54:37 -0500

    Full CRUD added working on edit page

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -11,7 +11,7 @@ router.get('/', async (req, res) => {
     // Look up the user from req.session
     const currentUser = await User.findById(req.session.user._id);
     // Render index.ejs, passing in all of the current user's
-    // applications as data in the context object.
+    // reviews as data in the context object.
     res.render('reviews/index.ejs', {
       reviews: currentUser.reviews,
     });
@@ -34,11 +34,11 @@ router.post('/', async (req, res) => {
     // Look up the user from req.session
     const currentUser = await User.findById(req.session.user._id);
     // Push req.body (the new form data object) to the
-    // applications array of the current user
+    // reviews array of the current user
     currentUser.reviews.push(req.body);
     // Save changes to the user
     await currentUser.save();
-    // Redirect back to the applications index view
+    // Redirect back to the reviews index view
     res.redirect(`/users/${currentUser._id}/reviews`);
   } catch (error) {
     // If any errors, log them and redirect back home
@@ -52,9 +52,9 @@ router.get('/:reviewId', async (req, res) => {
   try {
     // Look up the user from req.session
     const currentUser = await User.findById(req.session.user._id);
-    // Find the application by the applicationId supplied from req.params
+    // Find the review by the reviewId supplied from req.params
     const review = currentUser.reviews.id(req.params.reviewId);
-    // Render the show view, passing the application data in the context object
+    // Render the show view, passing the review data in the context object
     res.render('reviews/show.ejs', {
       review: review,
     });
@@ -71,14 +71,52 @@ router.delete('/:reviewId', async (req, res) => {
     // Look up the user from req.session
     const currentUser = await User.findById(req.session.user._id);
     // Use the Mongoose .deleteOne() method to delete
-    // an application using the id supplied from req.params
+    // a review using the id supplied from req.params
     currentUser.reviews.id(req.params.reviewId).deleteOne();
     // Save changes to the user
     await currentUser.save();
-    // Redirect back to the applications index view
+    // Redirect back to the reviews index view
     res.redirect(`/users/${currentUser._id}/reviews`);
   } catch (error) {
     // If any errors, log them and redirect back home
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
+// EDIT Route
+router.get('/:reviewId/edit', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    const review = currentUser.reviews.id(req.params.reviewId);
+    res.render('reviews/edit.ejs', {
+      review: review,
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
+// UPDATE Route
+
+router.put('/:reviewId', async (req, res) => {
+  try {
+    // Find the user from req.session
+    const currentUser = await User.findById(req.session.user._id);
+    // Find the current review from the id supplied by req.params
+    const review = currentUser.reviews.id(req.params.reviewId);
+    // Use the Mongoose .set() method
+    // this method updates the current review to reflect the new form
+    // data on `req.body`
+    review.set(req.body);
+    // Save the current user
+    await currentUser.save();
+    // Redirect back to the show view of the current review
+    res.redirect(
+      `/users/${currentUser._id}/reviews/${req.params.reviewId}`
+    );
+  } catch (error) {
     console.log(error);
     res.redirect('/');
   }
