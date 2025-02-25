0f9593387889cff806d7e13eed39e434f110827f
Shakiera Earvin <kieraearvin@gmail.com>
2025-02-24 22:32:34 -0500

    Built the delete route

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -65,6 +65,25 @@ router.get('/:reviewId', async (req, res) => {
   }
 });
 
+// DELETE Route 
+router.delete('/:reviewId', async (req, res) => {
+  try {
+    // Look up the user from req.session
+    const currentUser = await User.findById(req.session.user._id);
+    // Use the Mongoose .deleteOne() method to delete
+    // an application using the id supplied from req.params
+    currentUser.reviews.id(req.params.reviewId).deleteOne();
+    // Save changes to the user
+    await currentUser.save();
+    // Redirect back to the applications index view
+    res.redirect(`/users/${currentUser._id}/reviews`);
+  } catch (error) {
+    // If any errors, log them and redirect back home
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
 
 
 module.exports = router;
